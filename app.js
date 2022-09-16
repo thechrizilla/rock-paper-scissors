34fb1b99ea5233404fe6921f800b935c5cc10674
Chris Chan <chrisck.chan@hotmail.com>
2022-09-16 16:16:56 -0400

    finish ui for rock-paper-scissors

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 // variables
-let buttons = document.querySelectorAll(".button");
+const buttons = document.querySelectorAll(".button");
+
+const computerResults = document.createElement('div');
+const playerResults = document.createElement('div');
+const computerP = document.querySelector(".computer-points-title");
+const playerP = document.querySelector(".player-points-title");
+
+const computerChoice = document.createElement('div');
+const playerChoice = document.createElement('div');
+const computerC = document.querySelector(".computer-choice-title");
+const playerC = document.querySelector(".player-choice-title");
+
+const finalPoints = document.createElement('div');
+const finalResultsContainer = document.querySelector('.finals-container');
+
 const choices = ["rock", "paper", "scissors"];
 
 let playerPoints = 0;
 let computerPoints = 0;
-
 let playerSelection = "";
 
 
@@ -29,16 +42,50 @@ function playRound(playerSelection, computerSelection) {
     else {
         playerPoints++;
     }
-
 }
 
-// get player's choice
+// displaying results based on player's input
 buttons.forEach((button) => {
     console.log(button)
     button.addEventListener("click", () => {
-        playRound(button.id, getComputerChoice());
+        // get both computer and player choice
+        let cc = getComputerChoice();
+        let pc = button.id;
+
+        // play the game
+        playRound(pc, cc);
+
+        // showing choices
+        computerChoice.classList.add('choice');
+        playerChoice.classList.add('choice');
+        computerChoice.textContent = `${cc}`;
+        playerChoice.textContent = `${pc}`;
+        computerC.appendChild(computerChoice);
+        playerC.appendChild(playerChoice);
+
+        // showing points
+        computerResults.classList.add('points');
+        playerResults.classList.add('points');
+        computerResults.textContent = `${computerPoints}`;
+        playerResults.textContent = `${playerPoints}`;
+        computerP.appendChild(computerResults);
+        playerP.appendChild(playerResults);
+
+        // conditions for the end of the game
+        if (computerPoints === 5) {
+            finalPoints.classList.add("fpoints");
+            finalPoints.textContent = "YOU ARE A LOSER, COMPUTER WINS";
+            finalResultsContainer.appendChild(finalPoints);
+        } else if (playerPoints === 5 ) {
+            finalPoints.classList.add("fpoints");
+            finalPoints.textContent = "YOU WIN";
+            finalResultsContainer.appendChild(finalPoints);
+        } else {
+        }
     })
 })
+
+
 
 
 
